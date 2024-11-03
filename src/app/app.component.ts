41c69175340efd30acf37148513dccab4a881e96
Anushka Eshan <eanushka91@gmail.com>
2024-11-03 12:15:45 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { LoginPageComponent } from './login-page/login-page.component';
+import { HomePageComponent } from './common/home-page/home-page.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LoginPageComponent,HomePageComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
